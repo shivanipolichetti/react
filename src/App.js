9680f3298d121fbf12542ad7eb4ad55402ec6f3e
shivanipolichetti <shivanipolichetti2@gmail.com>
2024-04-01 23:15:23 +0530

    class

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Image from "./classcomponent/image/image"
+import Button from "./functioncomponent/button/button"
+import ImageComponent from './classcomponent/image/image';
+
+function App() {
+  return (
+     <div>
+      <ImageComponent/>
+      <br></br>
+      <Button/>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/classcomponent/image/image.jsx
@@ -0,0 +1,15 @@
+const { Component } = require("react");
+
+
+
+class ImageComponent extends Component{
+    render(){
+       return(
+        <>
+        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="store bag" width="100"></img>
+        </>
+       )
+    }
+}
+
+export default ImageComponent;
